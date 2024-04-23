602d11f9111263cd52b9577be8cc8bcca65bdaf2
AarniX <aarni86@gmail.com>
2024-04-23 11:39:31 +0300

    Worked on the darts rules and background

--- darts-scorekeeper/src/components/ScoreInput.js
+++ darts-scorekeeper/src/components/ScoreInput.js
@@ -7,24 +7,52 @@ export default function ScoreInput() {
   const [score, setScore] = useState('');
   const { state, dispatch } = useGame();
   const currentPlayer = state.players[state.currentTurn];
+  const currentPlayerScore = state.scores[currentPlayer];
 
   const handleScoreSubmit = (e) => {
     e.preventDefault();
-    // Convert input to an integer and ensure it's a valid score. //
     const scoreInt = parseInt(score, 10);
-    
-    // Check if the score is a number and positive //
-    if (!isNaN(scoreInt) && scoreInt > 0) {
-      dispatch({
-        type: 'UPDATE_SCORE',
-        payload: { playerName: currentPlayer, score: scoreInt }
-      });
-      setScore(''); // Reset the score input after submission
-    } else {
-      // Provide feedback for invalid input :)
-      alert('Please enter a valid score.');
+  
+    if (isNaN(scoreInt) || scoreInt < 0) {
+      alert('Please enter a valid positive number for the score.');
+      return;
     }
+    
+    if (scoreInt > 180) {
+      alert('The maximum score in a single round is 180.');
+      return;
+    }
+  
+    // Calculate what the new score would be
+    const potentialNewScore = currentPlayerScore - scoreInt;
+  
+    // Check if the new score would be 1, which is not allowed because it's not possible to finish on a double
+    if (potentialNewScore === 1) {
+      alert('Score cannot be reduced to 1 as the last dart must be a double.');
+      return;
+    }
+  
+    // Check if the score is being finished and if the final throw is a double
+    if (potentialNewScore === 0 && !isDouble(scoreInt)) {
+      alert('The final dart must land in a double.');
+      return;
+    }
+  
+    // Proceed with updating the score
+    dispatch({
+      type: 'UPDATE_SCORE',
+      payload: { playerName: currentPlayer, score: scoreInt }
+    });
+    setScore(''); // Reset the score input after submission
   };
+  
+  // Helper function to determine if the score is a double
+  function isDouble(score) {
+    // A double is an even number that is less than or equal to 40 (double 20)
+    // and not 50, which is the bullseye, not a double.
+    return score <= 40 && score % 2 === 0 && score !== 50;
+  }
+  
 
   // Render form only if the game has started, there are players, and it's someone's turn!? //
   if (!state.gameStarted || state.players.length === 0 || state.currentTurn === null) {
